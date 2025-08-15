a61d5f08b56c886cdd2ac4eab2caa79f666820d6
Sara Colombel <166294497+SaraColombel@users.noreply.github.com>
2025-08-15 16:20:25 +0200

    Update gestion_contact.py

--- gestion_contact.py
+++ gestion_contact.py
@@ -9,6 +9,13 @@ else:
 
 
 def add_contact():
+
+    """
+    Demande prénom, nom et numéro à l'utilisateur.
+    Vérifie que le numéro est numérique.
+    Ajoute le contact à la liste et sauvegarde dans contacts.json.
+    """
+
     fname = input("Enter contact first name : ")
     name = input("Enter contact name : ")
     number = input("Enter contact number : ")
@@ -23,6 +30,13 @@ def add_contact():
 
 
 def display_contacts():
+
+    """
+    Affiche tous les contacts de la liste.
+    Si la liste est vide, affiche un message.
+    Chaque contact est affiché avec prénom, nom et numéro.
+    """
+
     if not contacts :
         print("You have no friends.")
     else :
@@ -31,8 +45,11 @@ def display_contacts():
             print(f"{i}. {contact['fname']} {contact['name']} - {contact['number']}")
             print("-" * 30)
 
+
 choice = ""
 
+# Boucle principale du programme
+# Affiche le menu, exécute l'action choisie et demande si l'utilisateur veut revenir au menu
 while True:
     print("\nWelcome to your personal contact assistant ! What can I do for you today ?")
     print("1: Add a contact")
